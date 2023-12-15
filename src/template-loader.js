a7de0b1fdd4922d75a8dc683f26e4fe50ef1ba2f
maximalekseenko <max.alekseenko@gmail.com>
2023-12-15 15:28:12 +0300

    dynamic content loader added

--- src/template-loader.js
+++ src/template-loader.js
@@ -1,7 +1,21 @@
 $( function() {
+    // ---------- head ----------
     $.get( "./templates/head.html", function(content){
         $( ".template.head" ).prepend( content);
     });
+    // ---------- head - end ----------
+
+    // ---------- header ----------
     $( ".template.header" ).load( "./templates/header.html" );
+    // ---------- header - end ----------
+
+    // ---------- content ----------
+    $.get( `./pages/${ new URLSearchParams(window.location.search).get( "page" ) }.html`, function(content){
+        $( ".template.content" ).html( content );
+    });
+    // ---------- content - end ----------
+
+    // ---------- footer ----------
     $( ".template.footer" ).load( "./templates/footer.html" );
+    // ---------- footer - end ----------
 })
